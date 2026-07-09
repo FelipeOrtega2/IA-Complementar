748f1fa523f64d1bb4e98f14bca4cda31b44fba5
Felipe <felipeortegs8@gmail.com>
2026-07-08 21:50:29 -0300

    começo do java

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let chave = "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"
+let endereco = "https://api.groq.com/openai/v1/chat/completions"
+async function gerarCodigo() {
+let textarea = document.querySelector(".textarea").value 
+    console.log(textarea)
+}
