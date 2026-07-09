669fa107f0996e90613eac9fed4c4d33f49f1201
Felipe <felipeortegs8@gmail.com>
2026-07-09 15:33:10 -0300

    estilização por js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,34 @@
 let chave = "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"
 let endereco = "https://api.groq.com/openai/v1/chat/completions"
-async function gerarCodigo() {
-let textarea = document.querySelector(".textarea").value 
-    console.log(textarea)
+
+async function gerarCodigo(){
+    let textarea = document.querySelector(".textarea").value
+    let resposta = await fetch(endereco, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 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"
+        },
+        body: JSON.stringify({
+            "model": "llama-3.3-70b-versatile",
+            "messages": [
+                {
+                    "role" : "user",
+                    "content": textarea
+                },
+                {
+                    "role": "system",
+                    "content": "Você é um programador que ajuda a gerar códigos de negócios com base na descrição fornecida pelo usuário. responda com o código da pagina em HTML e CSS e sem comentários. sempre mostrando em português Brasileiro."
+                }
+            ]
+        })
+    });
+
+    let dados = await resposta.json()
+    let resultado = dados.choices[0].message.content
+    let codigoRetornado = document.querySelector(".codigo")
+    let SiteRetornado = document.querySelector(".Site")
+    SiteRetornado.srcdoc = resultado
+    codigoRetornado.textContent = resultado
+    console.log(resultado)
 }
